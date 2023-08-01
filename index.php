bc769ef91a0dc4fc40e839d36aad31e3d79f679b
balamachi <141099816+balamachi@users.noreply.github.com>
2023-08-01 13:13:51 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<HTML>
+
+<head>
+	<meta charset = "utf-8">  
+    <meta name = "viewport" content = "width=device-width, initial-scale = 1.0">  
+		<TITLE>	WELCOME GASC-KADALADI</TITLE>
+		<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+	<BODY>
+		<div class="container">
+			<table>
+				<tr>
+					<td>
+						<img src="logocollege.jpg" height="120px" width="140px">
+			
+					</td> 
+	 
+					<td width=1400 align=center>
+						<b>
+						<font size="6" color="880591"  align="center"> அரசு கலை மற்றும் அறிவியல் கல்லூரி-கடலாடி<br></font>
+       
+						<font size="4" color="red"> (காரைக்குடி, அழகப்பா பல்கலைக்கழகத்துடன் இணைக்கப்பட்டுள்ளது) </font><br>
+       
+						<font size="5" color="blue">  இராமநாதபுரம் மாவட்டம்-623703<br></font><hr color=blue border=solid>
+	  
+						<font size="5" color="512E5F">GOVERNMENT ARTS AND SCIENCE COLLEGE - KADALADI<br></font>
+        
+						<font size="4" color="red">(Affiliated to Alagappa University, Karaikudi, Tami Nadu)</font><br>
+     
+						<font size="5" color="blue">Ramanathapuram District-623703<br></font>
+						</b>
+					</td> 
+
+					<td>
+						<img src="logotamilnadu.png" height="120px" width="115px">
+			
+					</td>
+				</tr>
+			</table>	 
+		</div>
+
+	</BODY>
+<form action="" method="">
+<input type=text name=SName><br><br>
+<input type=Submit name=Submit color=red>
+
+</form>
+
+
+</HTML>
